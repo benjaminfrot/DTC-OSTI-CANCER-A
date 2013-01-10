b91195d9547dd9f80ba89b9ad21631da6de311be
Benjamin Frot <benjamin.frot@gmail.com>
2013-01-10 15:57:16 +0000

    Bug Fix for ATPUpdate
    Variables cannot be defined as global and have to be given as inputs to
    the functions.

--- ATPUpdate.m
+++ ATPUpdate.m
@@ -1,4 +1,4 @@
-function [ATP] = ATPUpdate(Glucose,Oxygen)
+function [ATP] = ATPUpdate(params,Glucose,Oxygen)
 %% Update the ATP matrix with both aerobic and anaerobic processes.
 % State : A State matrix of size NxM with entries from 1 to 9
 % Glucose : A Glucose matrix of size NxM. \phi_g
@@ -8,4 +8,4 @@ function [ATP] = ATPUpdate(Glucose,Oxygen)
 % Also, n = 2/params.na (paper P.711)
 tmp = Glucose - Oxygen;
 mask = tmp >= 0;
-ATP = Oxygen + 2/params.na * (mask .* tmp);
+ATP = Oxygen + 2/(params.na) * (mask .* tmp);
---
+++ setParams.m
@@ -0,0 +1,19 @@
+% Input : params, a structure with the following fields :
+%    params.k : glycolytic rate (e.g. k = 10)
+%    params.ht : tumour cell acidity threshold (should be 8.6 10^3)
+%    params.hn : normal cell acidity thereshold (should be 9.3 10^2)
+%    params.pa : Adaptation rate (10^-3)
+%    params.width : Width of the grid (50)
+%    params.height : Height of the grid 
+%    params.na : Number of ATP molecules produced during complete oxydation
+%    (Eq (1) of the paper). Typically 36
+%    params.a0 : Minimum ATP for a cell to survive. Typically 0.1
+
+params.k = 10;
+params.ht = 8600;
+params.hn = 930;
+params.pa = 0.001;
+params.width = 50;
+params.height = 200;
+params.na = 36;
+params.a0 = 0.1;
