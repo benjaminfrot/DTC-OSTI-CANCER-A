057292589d403d218044b2fc5f466870d6b08206
Benjamin Frot <benjamin.frot@gmail.com>
2013-01-10 15:36:59 +0000

    Add ATPUpdate function
    Take the Oxygen (\phi_c) and Glucose (\phi_g) concentrations and returns
    ATP (\phi_a).

---
+++ ATPUpdate.m
@@ -0,0 +1,11 @@
+function [ATP] = ATPUpdate(Glucose,Oxygen)
+%% Update the ATP matrix with both aerobic and anaerobic processes.
+% State : A State matrix of size NxM with entries from 1 to 9
+% Glucose : A Glucose matrix of size NxM. \phi_g
+% Oxygen : A matrix of size NxM. \phi_c = C = c (because C_X = 1)
+
+% \phi_a = c + n(\phi_g -c) . But here c = C/Cx and Cx = 1.
+% Also, n = 2/params.na (paper P.711)
+tmp = Glucose - Oxygen;
+mask = tmp >= 0;
+ATP = Oxygen + 2/params.na * (mask .* tmp);
