c2693066686f16c0ebe216337b27e90e176fc945
Annekathrin Meiburg <meiburg@DTC127.(none)>
2013-01-11 11:43:31 +0000

    add diffusion

--- setParams.m
+++ setParams.m
@@ -9,6 +9,7 @@
 %    (Eq (1) of the paper). Typically 36
 %    params.a0 : Minimum ATP for a cell to survive. Typically 0.1
 %    params.dg : 1/dg^2, where dg is diffusion coefficient for glucose. (1.3*10^2)
+%    params.dc : 1/dc^2, where dc is the diffusion coefficient for oxygen.
 
 params.k = 10;
 params.ht = 8600;
@@ -19,3 +20,4 @@ params.height = 50;
 params.na = 36;
 params.a0 = 0.1;
 params.dg = 1/130^2;
+params.dc = 1/25;
