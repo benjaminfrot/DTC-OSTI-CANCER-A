73e02320732ac7b3d403e80a90a61d853d5367e5
Samuel Demharter <samuel.demharter@seh.ox.ac.uk>
2013-01-14 11:58:08 +0000

    Added coments to InitialiseProblem

--- Code/InitialiseProblem.m
+++ Code/InitialiseProblem.m
@@ -13,7 +13,7 @@
    
 function [State,ATP,Glucose,Oxygen,Hydrogen] = InitialiseProblem(params)
 %% Initialise the problem using the params structure
-% Input : params, a structure with the following fields :
+% Input : params, a structure with the following fields (values taken from Smallbone et al., 2007) :
 %    params.k : glycolytic rate (e.g. k = 10)
 %    params.ht : tumour cell acidity threshold (should be 8.6 10^3)
 %    params.hn : normal cell acidity thereshold (should be 9.3 10^2)
@@ -24,24 +24,24 @@ function [State,ATP,Glucose,Oxygen,Hydrogen] = InitialiseProblem(params)
 %    (Eq (1) of the paper). Typically 36
 %    params.a0 : Minimum ATP for a cell to survive. Typically 0.1
 %    params.dg : 1/dg^2, where dg is the diffusion coefficient for glucose. (dg=1.3*10^2)
-%    params.dc : 1/dc^2, where dc is the diffusion coefficient for Oxygen. (dc=5)
+%    params.dc : 1/dc^2, where dc is the diffusion coefficient for oxygen. (dc=5)
 
-% A cell can have one of 9 states : Empty or a combination of all these
-%states : hyperplastic, acid-resitant, glycolytic. In this implementation
-% the state of the cell is described by a number between 0 and 8.
+% A cell can have one of 9 states : Empty or up to 3 mutations:
+% hyperplastic, acid-resitant, glycolytic.
+% In this implementation the state of the cell is described by a number
+% between 0 and 8.
 % 0 : Empty
 % 1 : Normal
 % x : decimal representation of the binary number described by
-% Hyperplastic Acid-Resistant Glycolytic plus one.
-%For example, if the cell is Hyperplastic and Glycolytic it is 101 --> 5 +
-%1 = 6
+% Hyperplastic acid-resistant glycolytic plus one.
+% For example, if the cell is hyperplastic and glycolytic it is
+%     101 --> 5 + 1 = 6
 
-%Initialise by setting a layer of normal cells. The rest is empty.
+% Initialise by setting a layer of normal cells. The rest is empty.
 State = zeros(params.height,params.width);
 State(params.height,:) = 1; %Start with normal cells 000 + 1
 
-%All initial levels are set to one and any changes are just expressed
-%as fractions of 1.
+% Initial conditions
 ATP = zeros(params.height,params.width);
 ATP(params.height,:) = 1;
 
