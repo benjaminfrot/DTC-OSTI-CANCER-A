0cd3fbf4f077c51db6da10eb02f489924b3882f1
Samuel Demharter <samuel.demharter@seh.ox.ac.uk>
2013-01-14 14:39:54 +0000

    added comments to Mutate

--- Code/Mutate.m
+++ Code/Mutate.m
@@ -18,10 +18,15 @@ function [ y ] = Mutate(params, parentState)
 
     y = parentState;
     u = rand(1);
+    
+    % preparing conversion of State to binary
     parentState = parentState - 1;
+    
+    % Convert back to binary
     binaryCoeffs = [ mod(floor(parentState/4),2) mod(floor(parentState/2),2) mod(parentState,2)];
     
-    if(params.pa > u)   % if true then mutate
+    % randomly pick one of the three mutations with likelihood pa
+    if(params.pa > u)
         bitPosition = randsample(3,1);
         binaryCoeffs(bitPosition) = mod(binaryCoeffs(bitPosition) + 1,2);
         y = binaryCoeffs(1)*4 + binaryCoeffs(2)*2 + binaryCoeffs(3) + 1;
